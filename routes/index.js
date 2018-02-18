8acbd0fb9f53d2b74c4133d50c4717b8694aa27a
Yuanchen Lu <ylu36@ncsu.edu>
2018-02-18 00:29:54 -0500

    signup redirect

--- routes/index.js
+++ routes/index.js
@@ -20,25 +20,25 @@ router.get("/register", function(req, res){
 //handle sign up logic
 router.post("/register", function(req, res){
     console.log(req.body);
-    // var newUser = new User({username: req.body.username});
-    // newUser.save(newUser, req.body.password, function(err, user){
-    //     if(err){
-    //         console.log(err);
-    //         return res.render("register", {error: err.message});
-    //     }
-    //     passport.authenticate("local")(req, res, function(){
-    //         req.flash("success", "Welcome " + user.username + "!");
-    //         res.redirect("/");
-    //     });
-    // });
-    var data = new User({username: req.body.username, password: req.body.password});
-
-    data.save(function (err, data) {
-        console.log(data);
-        console.log(data.username + " has " + data.password);
-        req.flash("success", "Welcome " + data.username + "!");
-        res.redirect("/");
+    var newUser = new User({username: req.body.username});
+    newUser.save(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render("register", {error: err.message});
+        }
+        passport.authenticate("local")(req, res, function(){
+            req.flash("success", "Welcome " + user.username + "!");
+            res.redirect("/");
+        });
     });
+    // var data = new User({username: req.body.username, password: req.body.password});
+    //
+    // data.save(function (err, data) {
+    //     console.log(data);
+    //     console.log(data.username + " has " + data.password);
+    //     //req.flash("success", "Welcome " + data.username + "!");
+    //     res.redirect("/");
+    // });
 });
 
 // show login form
@@ -48,9 +48,10 @@ router.get("/login", function(req, res){
 // handling login logic
 router.post("/login", passport.authenticate("local",
     {
-        successRedirect: "/",
+        successRedirect: "/page",
         failureRedirect: "/login"
     }), function(req, res){
+    console.log(req.body);
 });
 
 // logic route
