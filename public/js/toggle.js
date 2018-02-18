4229486f5c4463911440ab50fe8c86774c4f75f8
Skycocoo <luoyx2014@163.com>
2018-02-18 06:20:31 -0500

    Finish personal budget

--- public/js/toggle.js
+++ public/js/toggle.js
@@ -1,8 +1,10 @@
 var Toggle = {
     $menu: $("#pub-bul-sub1"),
     $menu2: $("#pub-bul-sub2"),
+    $menu3: $("#usr-bul-sub1"),
     $tri: $("#pub-main1"),
     $tri2: $("#pub-main2"),
+    $tri3: $("#usr-main1"),
 
     init: function(){
         var self = this;
@@ -13,6 +15,11 @@ var Toggle = {
         self.$tri2.click(function(){
             self.$menu2.toggleClass("active");
             self.$tri2.toggleClass("rot");
+        });
+        self.$tri3.click(function(){
+            self.$menu3.toggleClass("active");
+            self.$tri3.toggleClass("rot");
+            console.log("hi");
         });
     },
 };
