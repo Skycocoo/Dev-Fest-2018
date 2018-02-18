ef2f0e5d328ded340091a9072cb06a26f4c368e1
Skycocoo <luoyx2014@163.com>
2018-02-18 06:54:07 -0500

    Finish personal display

--- public/js/toggle.js
+++ public/js/toggle.js
@@ -2,9 +2,13 @@ var Toggle = {
     $menu: $("#pub-bul-sub1"),
     $menu2: $("#pub-bul-sub2"),
     $menu3: $("#usr-bul-sub1"),
+    $menu4: $("#usr-bul-sub2"),
+    $menu5: $("#usr-bul-sub3"),
     $tri: $("#pub-main1"),
     $tri2: $("#pub-main2"),
     $tri3: $("#usr-main1"),
+    $tri4: $("#usr-main2"),
+    $tri5: $("#usr-main3"),
 
     init: function(){
         var self = this;
@@ -19,7 +23,14 @@ var Toggle = {
         self.$tri3.click(function(){
             self.$menu3.toggleClass("active");
             self.$tri3.toggleClass("rot");
-            console.log("hi");
+        });
+        self.$tri4.click(function(){
+            self.$menu4.toggleClass("active");
+            self.$tri4.toggleClass("rot");
+        });
+        self.$tri5.click(function(){
+            self.$menu5.toggleClass("active");
+            self.$tri5.toggleClass("rot");
         });
     },
 };
